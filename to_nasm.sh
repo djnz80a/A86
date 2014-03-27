d2a2d09448fd032eb1678a3332b1d5e4c083aa07
djnz80a <djnz80a@gmail.com>
2014-03-28 05:25:57 +0900

    add comment

--- to_nasm.sh
+++ to_nasm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # replace "DEFS n" to "TIME n DB 0"
+# replace "ES:[BX(+n)]" to "[ES:BX(+n)]"
 # -b to "not convert crlf with cygwin sed"
 cat A86.ASM |sed -b -e 's/\bDEFS[ \t]\+\([^;]*\)\b/TIMES \1 DB 0/' |sed -b -e "s/ES:\[BX\(.*\)\]/[ES:BX\1]/" > A86NASM.ASM
